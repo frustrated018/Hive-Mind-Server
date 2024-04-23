6d0ee9619c67631c147d00c53007c74506e2f2d5
frustrated018 <niloyd018@gmail.com>
2024-04-23 20:47:27 +0600

    fixed typo

--- src/Api/Assignment/findAllAssignments.js
+++ src/Api/Assignment/findAllAssignments.js
@@ -4,7 +4,7 @@ const findAllAssignments = async (req, res) => {
   const result = await Assignment.aggregate([
     {
       $project: {
-        thumbthumbnail: 1,
+        thumbnail: 1,
         title: 1,
         difficulty: 1,
         description: 1,
